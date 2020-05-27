c5dee07e98ec6820e1e842ae936d17e9349c2d1b
Bollpiska <bollpiska@hotmail.com>
2020-05-27 20:52:03 +0200

    stilsatt medlemsformuläret lite

--- src/views/Medlem.vue
+++ src/views/Medlem.vue
@@ -22,12 +22,29 @@
     <div>
         <form @submit="sendForm">
             <div id="form-div">
-                <label>Förnamn<input type="text" v-model="firstname"></label>
-                <label>Efternamn<input type="text" v-model="lastname"></label>
-                <label>Address<input type="text" v-model="address"></label>
-                <label>Postnummer<input type="text" v-model="zipcode"></label>
-                <label>Postort<input type="text" v-model="city"></label>
-                <label><input id="skicka-btn" type="submit" value="Skicka"></label>
+                <div class="form_item">
+                    <label class="form-label">Förnamn</label>
+                    <input type="text" v-model="firstname" placeholder="Förnamn">
+                </div>
+                <div class="form_item">
+                    <label class="form-label">Efternamn</label>
+                    <input type="text" v-model="lastname" placeholder="Efternamn">
+                </div>                
+                <div class="form_item">
+                    <label class="form-label">Address</label>
+                    <input type="text" v-model="address" placeholder="Gatuadress">
+                </div>
+                <div class="form_item">
+                    <label class="form-label">Postnummer</label>
+                    <input type="text" v-model="zipcode" placeholder="Postnummer">
+                </div>
+                <div class="form_item">
+                    <label class="form-label">Postort</label>
+                    <input type="text" v-model="city" placeholder="Stad">
+                </div>
+                <div class="form_item">
+                    <label></label><input id="skicka-btn" type="submit" value="Skicka">
+                </div>
             </div>
         
         </form>
@@ -77,16 +94,56 @@ export default {
 
 
     #form-div {
+        font-family: sans-serif;
+        font-size: 16px;
         display: flex;
         flex-direction: column;
+        box-sizing: border-box;
+        line-height: 1.5;
 
-        #skicka-btn {
-            border-radius: 0%;
-            padding: 6px;
-            background-color: red;
-            color: white;
+
+        .form_item {
+            display:flex;
+            flex-direction: column;
+
         }
+
+        .form_item > *{
+            align-self: flex-start;
+        }
+
+        .form-label {
+            font-weight: bolder;
+            padding: 10px 0;
+        }  
+
      
+    }
+
+    input {
+        -webkit-appearance: none;
+        width: 100%;
+        max-width: 400px;
+        border: 2px solid #dddddd;
+        border-radius: 4px;
+        background: #f9f9f9;
+        padding: 0.8em;
+        font-size: 0.9em;
+    
+
+    }
+
+    #skicka-btn {
+        font-family: sans-serif;
+        font-weight: bolder;
+        font-size: 1.1em;
+        padding: 10px 10px;
+        margin-top: 10px;
+        width: 100px;
+        background-color: red;
+        color: white;
+        border-radius: 3px;
+        border: 2px solid rgb(145, 10, 10)
     }
 
 </style>
